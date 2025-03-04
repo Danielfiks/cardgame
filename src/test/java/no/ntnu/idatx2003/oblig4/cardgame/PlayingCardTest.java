1a7e6a0abf4d8c10319e39fbfaa06f95d4a25ae0
Daniel Wegner Fiksdalstrand <danielwf@stud.ntnu.no>
2025-03-04 17:58:01 +0100

    PlayingCardTest.java v-0.0.1
    Added class for testing PlayingCardTest.java

---
+++ src/test/java/no/ntnu/idatx2003/oblig4/cardgame/PlayingCardTest.java
@@ -0,0 +1,11 @@
+package no.ntnu.idatx2003.oblig4.cardgame;
+
+/**
+ * This class is for testing the {@link PlayingCard} class.
+ * @author Daniel Wegner Fiksdalstrand
+ * @version 0.0.1
+ * @since 0.0.1 (04.03.25)
+ */
+class PlayingCardTest {
+  
+}
