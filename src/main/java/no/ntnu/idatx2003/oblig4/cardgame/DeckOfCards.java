fedd8c39dc6ff1a9bfa792b32e36fe0bc22e9912
Daniel Wegner Fiksdalstrand <danielwf@stud.ntnu.no>
2025-03-05 11:30:16 +0100

    DeckOfCards.java v-0.0.1
    Added a class for holding the deck of cards.

---
+++ src/main/java/no/ntnu/idatx2003/oblig4/cardgame/DeckOfCards.java
@@ -0,0 +1,24 @@
+package no.ntnu.idatx2003.oblig4.cardgame;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class is responsible for holding al the 52 cards.
+ * @author Daniel Wegner Fiksdalstrand
+ * @version 0.0.1
+ * @since 0.0.1 (05.03.25).
+ */
+public class DeckOfCards {
+  private final char[] suits = { 'S', 'H', 'D', 'C' };
+  private List<String> cards;
+
+  public DeckOfCards() {
+    this.cards = new ArrayList<>();
+  }//13 cards for each color for loop
+
+
+
+
+
+}
