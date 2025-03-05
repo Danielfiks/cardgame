22f272f8d7073d7d3dc51643107be8cdb795a6c8
Daniel Wegner Fiksdalstrand <danielwf@stud.ntnu.no>
2025-03-05 13:03:04 +0100

    DeckOfCards.java v-0.0.2
    Added method for filling array with cards. Also refactored code.

--- src/main/java/no/ntnu/idatx2003/oblig4/cardgame/DeckOfCards.java
+++ src/main/java/no/ntnu/idatx2003/oblig4/cardgame/DeckOfCards.java
@@ -6,19 +6,37 @@ import java.util.List;
 /**
  * This class is responsible for holding al the 52 cards.
  * @author Daniel Wegner Fiksdalstrand
- * @version 0.0.1
+ * @version 0.0.2
  * @since 0.0.1 (05.03.25).
  */
 public class DeckOfCards {
   private final char[] suits = { 'S', 'H', 'D', 'C' };
-  private List<String> cards;
+  private List<PlayingCard> deck;
 
+  /**
+   * The DeckOfCards constructor.
+   */
   public DeckOfCards() {
-    this.cards = new ArrayList<>();
-  }//13 cards for each color for loop
+    this.deck = new ArrayList<>();
+    fillDeckWithCards();
+  }
 
+  private void fillDeckWithCards() {
+    int card;
 
+    for (char suit : this.suits) {
+      for (card = 1; card < 14; card++) {
+        this.deck.add(new PlayingCard(suit, card));
+      }
+    }
 
+    //Used for testing: Printing the arrayList.
+    /*
+    for (PlayingCard playingCard : this.deck) {
+      System.out.println(playingCard.getAsString());
+    }
+     */
 
+  }
 
 }
