5ff4e2e4e1dbb0f875d2eed0cc3c488ed8b69821
Daniel Wegner Fiksdalstrand <danielwf@stud.ntnu.no>
2025-03-09 01:07:42 +0100

    Main.java v-0.0.1
    Added a main method for running the code.

---
+++ src/main/java/no/ntnu/idatx2003/oblig4/cardgame/Main.java
@@ -0,0 +1,14 @@
+package no.ntnu.idatx2003.oblig4.cardgame;
+
+/**
+ * Is the Main method for running the program.
+ * @author Daniel Wegner Fiksdalstrand
+ * @version 0.0.1
+ * @since 0.0.1 (09.03.25)
+ */
+public class Main {
+  public static void main (String []args) {
+    DeckOfCards game = new DeckOfCards();
+
+  }
+}
