27f2de5d1b772bf5d1ea37da407378f0bf18d6af
Daniel Wegner Fiksdalstrand <danielwf@stud.ntnu.no>
2025-03-09 01:09:32 +0100

    DeckOfCardsTest.java v-0.0.1
    Added for testing the DeckOfCards class.

---
+++ src/test/java/no/ntnu/idatx2003/oblig4/cardgame/DeckOfCardsTest.java
@@ -0,0 +1,29 @@
+package no.ntnu.idatx2003.oblig4.cardgame;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * This class is for testing the {@link DeckOfCards} class.
+ * @author Daniel Wegner Fiksdalstrand
+ * @version 0.0.1
+ * @since 0.0.1 (09.03.25)
+ */
+class DeckOfCardsTest {
+  DeckOfCards deck;
+
+  @BeforeEach
+  void init() {
+    this.deck = new DeckOfCards();
+  }
+
+//  @Test
+//  void checkFillDeckWithCards() {
+//    this.deck.
+//    this.deck.fillDeckWithCards();
+//
+//    Assertions.assertTrue(this.deck.);
+//  }
+
+}
