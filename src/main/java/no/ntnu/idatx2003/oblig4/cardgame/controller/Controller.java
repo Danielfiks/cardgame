79272ecce558aba078119cfbc8da383a7d8caf37
Daniel Wegner Fiksdalstrand <danielwf@stud.ntnu.no>
2025-03-12 00:44:20 +0100

    Controller.java v-0.0.1
    Created a class handling the business logic

---
+++ src/main/java/no/ntnu/idatx2003/oblig4/cardgame/controller/Controller.java
@@ -0,0 +1,37 @@
+package no.ntnu.idatx2003.oblig4.cardgame.controller;
+
+import no.ntnu.idatx2003.oblig4.cardgame.model.DeckOfCards;
+import no.ntnu.idatx2003.oblig4.cardgame.model.PlayingCard;
+import no.ntnu.idatx2003.oblig4.cardgame.view.View;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Controller {
+  // MVC
+  private final DeckOfCards model;
+  private final View view;
+
+  public Controller(DeckOfCards model, View view) {
+    this.model = model;
+    this.view = view;
+  }
+
+  public void dealHand() {
+    this.model.dealHand(5);
+  }
+
+  public void checkHand() {
+    List<String> dealtHand = this.model.getDealtHandAsString();
+    String stringOfCards = dealtHand.stream().collect(Collectors.joining(" "));
+    this.view.updateHand(stringOfCards);
+    calculateSumOnHand();
+  }
+
+  public void calculateSumOnHand() {
+    List<Integer> dealtHand = this.model.getDealtHand().stream().map(PlayingCard::getFace).toList();
+    int sum = dealtHand.stream().mapToInt(Integer::intValue).sum();
+    this.view.updateSumOfFaces("" + sum);
+  }
+
+}
