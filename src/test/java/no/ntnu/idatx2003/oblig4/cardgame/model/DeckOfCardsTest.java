ba406b3f020595466bce0ddb39979966b6fecaef
Daniel Wegner Fiksdalstrand <danielwf@stud.ntnu.no>
2025-03-12 00:41:10 +0100

    DeckOfCards.java v-0.0.8
    Added 2 methods. One for getting a list from the Hand class and the other for retruning a list of strings.

--- src/test/java/no/ntnu/idatx2003/oblig4/cardgame/model/DeckOfCardsTest.java
+++ src/test/java/no/ntnu/idatx2003/oblig4/cardgame/model/DeckOfCardsTest.java
@@ -1,8 +1,6 @@
-package no.ntnu.idatx2003.oblig4.cardgame;
+package no.ntnu.idatx2003.oblig4.cardgame.model;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 /**
  * This class is for testing the {@link DeckOfCards} class.
